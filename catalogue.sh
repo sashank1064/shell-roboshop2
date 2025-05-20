97824e3c9831c6e2e6552144e407c20ef4d92830
msivakumarreddy <info@joindevops.com>
2025-05-20 08:14:27 +0530

    hello world started

--- catalogue.sh
+++ catalogue.sh
@@ -51,7 +51,7 @@ else
     echo -e "System user roboshop already created ... $Y SKIPPING $N"
 fi
 
-mkdir /app 
+mkdir -p /app 
 VALIDATE $? "Creating app directory"
 
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip &>>$LOG_FILE
