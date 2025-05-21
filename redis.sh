8452ea1d1e1268e3390d401127540912c040aa66
msivakumarreddy <info@joindevops.com>
2025-05-21 07:45:57 +0530

    hello world started

--- redis.sh
+++ redis.sh
@@ -54,5 +54,5 @@ VALIDATE $? "Started Redis"
 END_TIME=$(date +%s)
 TOTAL_TIME=$(( $END_TIME - $START_TIME ))
 
-echo -e "Script exection completed successfully, $Y time taken: $TOTAL_TIME $N" | tee -a $LOG_FILE
+echo -e "Script exection completed successfully, $Y time taken: $TOTAL_TIME seconds $N" | tee -a $LOG_FILE
 
